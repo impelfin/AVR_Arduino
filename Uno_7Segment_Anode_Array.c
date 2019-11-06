9983c95cfec252fdfbe89525df83a43b3e0ad451
JunHyeon Mun <impelfin@gmail.com>
2019-11-06 15:18:32 +0900

    Update Uno_7Segment_Anode_Array.c

--- Uno_7Segment_Anode_Array.c
+++ Uno_7Segment_Anode_Array.c
@@ -1,35 +1,14 @@
 /*
  * Uni_7Segment_Anode_Array.c
  *
- * Created: 2017-11-19 ¿ÀÈÄ 6:18:51
+ * Created: 2017-11-19 Â¿Ã€ÃˆÃ„ 6:18:51
  * Author : Moon
  */ 
 
-#define F_CPU 16000000UL
+#define F_CPU 16E6
 
 #include <avr/io.h>
 #include <util/delay.h>
-
-#define D0 PD0
-#define D1 PD1
-#define D2 PD2
-#define D3 PD3
-#define D4 PD4
-#define D5 PD5
-#define D6 PD6
-#define D7 PD7
-#define D8 PB0
-#define D9 PB1
-#define D10 PB2
-#define D11 PB3
-#define D12 PB4
-#define D13 PB5
-#define A0 PC0
-#define A1 PC1
-#define A2 PC2
-#define A3 PC3
-#define A4 PC4
-#define A5 PC5
 
 #define DEBOUCE_MS 500
 
@@ -45,8 +24,6 @@ int main(void)
 {
 	DDRB = 0xFF;
 	DDRD = 0xFF;
-	PORTB = 0x00;
-	PORTD = 0x00;
 	
 	while(1) {
 		for (int i=0; i<10; i++) {
