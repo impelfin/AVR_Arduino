4a7bb454382057ff51e1fc7a4eaea6de8d02e23b
JunHyeon Mun <impelfin@gmail.com>
2019-11-06 15:19:59 +0900

    Update Uno_7Segment_Anode_7447.c

--- Uno_7Segment_Anode_7447.c
+++ Uno_7Segment_Anode_7447.c
@@ -1,19 +1,18 @@
 /*
  * Uno_7Segment_Anode_7447.c
  *
- * Created: 2017-11-19 ¿ÀÈÄ 7:08:45
+ * Created: 2017-11-19 Â¿Ã€ÃˆÃ„ 7:08:45
  * Author : Moon
  */ 
 
-#define F_CPU 16000000UL
+#define F_CPU 16E6
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define DEBOUCE_MS 500
 
-int main(void)
-{
+int main(void) {
 	DDRB = 0xFF;
 	PORTB = 0x00;
 	
