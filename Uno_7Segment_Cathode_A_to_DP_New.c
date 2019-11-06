86043d8eeb44c23ca486efadbb851c6533b1cdba
JunHyeon Mun <impelfin@gmail.com>
2019-11-06 13:56:35 +0900

    Create Uno_7Segment_Cathode_A_to_DP_New.c

---
+++ Uno_7Segment_Cathode_A_to_DP_New.c
@@ -0,0 +1,25 @@
+#define F_CPU 16E6
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DEBOUCE_MS 500
+#define NUMBER 8
+
+int main(void) {
+	DDRB = 0x0F;
+	DDRD = 0xF0;
+	
+	while (1) {
+		for (int i = 0; i <= NUMBER; ++i) {
+			if (i < 4) {
+				PORTD |= 0x10 << i;
+			} else {
+				PORTB |= 0x01 << (i % 4);
+			}
+			_delay_ms(DEBOUCE_MS);
+		}
+		PORTB = 0x00;
+		PORTD = 0x00;
+	}
+}
