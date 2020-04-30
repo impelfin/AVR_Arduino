c4981222cd9a09a5115d71f8eb55aa958f25ad7f
JunHyen Mun <impelfin@gmail.com>
2020-04-30 10:48:55 +0900

    no message

---
+++ 3led_with_Bit_Lable2.c
@@ -0,0 +1,24 @@
+#define F_CPU 16E6
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB = 1<<DDB2 | 1<<DDB1 | 1<<DDB0;
+	
+	while (1)
+	{
+		PORTB = PORTB & 0x0;
+
+		PORTB = 1<<PB0;	
+		_delay_ms(1000);
+		
+		PORTB = PORTB & ~(1<<PB0) | 1<<PB1;
+		_delay_ms(1000);
+		
+		PORTB = PORTB & ~(1<<PB1) | 1<<PB2;
+		_delay_ms(1000);
+	}
+}
+
