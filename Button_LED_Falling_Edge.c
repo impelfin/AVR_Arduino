3880252ddf372c6701a1987c6d4de2c68e57307b
JunHyeon Mun <impelfin@gmail.com>
2020-05-06 14:43:01 +0900

    Update Button_LED_Falling_Edge.c

--- Button_LED_Falling_Edge.c
+++ Button_LED_Falling_Edge.c
@@ -10,19 +10,19 @@ int main(void) {
 	
 	unsigned char o_sw, n_sw, led_state=0;
 	
-	o_sw = PINB & (1<<PINB1);
+	o_sw = PINB & (1<<PB1);
 	
 	while (1) {
-		n_sw = PINB & (1<<PINB1);
-		if(o_sw != 0 && n_sw == 0) {
-			if(led_state == 0) {
-				PORTB |= (1<<PB0) | (1<<PB5);
-				led_state = 1;
-			} else {
-				PORTB &= ~((1<<PB0) | (1<<PB5));
-				led_state = 0;
-			}
-		}
+		n_sw = PINB & (1<<PB1);
+		
+		if(o_sw != 0 && n_sw == 0) led_state =! led_state;
+			
 		o_sw = n_sw;
+	
+		if(led_state == 0) {
+			PORTB |= (1<<PB0) | (1<<PB5);
+		} else {
+			PORTB &= ~((1<<PB0) | (1<<PB5));
+		}
 	}
 }
