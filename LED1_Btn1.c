b43d7330ad8b9abdb6ae68d306fc601fe8190def
JunHyeon Mun <impelfin@gmail.com>
2020-05-06 22:21:21 +0900

    Update LED1_Btn1.c

--- LED1_Btn1.c
+++ LED1_Btn1.c
@@ -1,37 +1,21 @@
 #define F_CPU 16E6
 
-#define S0	PD5
-#define S1	PD6
-#define S2	PD7
-
-#define D0	PB0
-#define D1	PB1
-#define D2	PB2
-#define D3	PB3
-#define D4	PB4
-
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
-	DDRB |= 1<<D4 | 1<<D3 | 1<<D2 | 1<<D1 | 1<<D0;
-	DDRD &= ~(1<<S2) & ~(1<<S1) & ~(1<<S0);
+    DDRB = DDRB | (1<<PB0);
+	DDRD = DDRD & ~(1<<PD5);
 	
-	while (1) {
-		if(!(PIND & (1<<S0))) {
-			PORTB |= 1<<D1 | 1<<D0;
-			} else {
-			PORTB &= ~(1<<D1) & ~(1<<D0);
+	int sw;
+	
+    while (1) {
+		sw = PIND & (1<<PD5);
+		
+		if(sw == 0) {
+			PORTB = PORTB | (1<<PB0);
+		} else {
+			PORTB = PORTB & ~(1<<PB0);
 		}
-		if(!(PIND & (1<<S1))) {
-			PORTB |= 1<<D2;
-			} else {
-			PORTB &= ~(1<<D2);
-		}
-		if(!(PIND & (1<<S2))) {
-			PORTB |= 1<<D4 | 1<<D3;
-			} else {
-			PORTB &= ~(1<<D4) & ~(1<<D3);
-		}
-	}
+    }
 }
-
