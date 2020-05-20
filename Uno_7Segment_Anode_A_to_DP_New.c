b3e69459fb5ef518326d2f5046abb3b7c0e53cae
JunHyeon Mun <impelfin@gmail.com>
2020-05-20 23:11:16 +0900

    Update Uno_7Segment_Anode_A_to_DP_New.c

--- Uno_7Segment_Anode_A_to_DP_New.c
+++ Uno_7Segment_Anode_A_to_DP_New.c
@@ -12,15 +12,15 @@ int main(void) {
 	PORTD = 0xF0;
 	
     while (1) {
-		for(int i=0;i<8; ++i) {
-			if(i<4) {
-				PORTD = 0xE0<<i;	// 0b11100000;
-			} else {
-				PORTB =	0xFE<<(i%4);	// 0b11111110;
-			}
-			_delay_ms(DEBOUCE_MS);
+	for(int i=0;i<8; ++i) {
+		if(i<4) {
+			PORTD = 0xE0<<i;	// 0b11100000;
+		} else {
+			PORTB =	0xFE<<(i%4);	// 0b11111110;
 		}
-		PORTB = 0xFF;
-		PORTD = 0xFF;
+		_delay_ms(DEBOUCE_MS);
 	}
+	PORTB = 0xFF;
+	PORTD = 0xFF;
+    }
 }
